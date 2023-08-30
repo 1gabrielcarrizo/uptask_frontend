c6dc448ea26a698b5278c97d3d541ed7b244edce
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-30 17:25:32 -0300

    adding styles to new password and confirm account pages

--- src/pages/NuevoPassword.jsx
+++ src/pages/NuevoPassword.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+
 const NuevoPassword = () => {
   return (
-    <div>NuevoPassword</div>
+    <>
+      <h1 className='text-sky-600 font-black text-6xl capitalize'>Reestablece tu password y no pierdas acceso a tus <span className='text-slate-700'>proyectos</span></h1>
+
+      <form className='my-10 bg-white shadow rounded-lg p-10'>
+        <div className='my-5'>
+          <label
+            htmlFor="password"
+            className='uppercase text-gray-600 block text-xl font-bold'>
+            Nuevo Password
+          </label>
+          <input
+            type="password"
+            id='password'
+            placeholder='Escribe tu Nuevo Password'
+            className='w-full mt-3 p-3 border rounded-xl bg-gray-50' />
+        </div>
+
+        <input
+          type="submit"
+          value="Guardar Nuevo Password"
+          className='bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-5' />
+      </form>
+    </>
   )
 }
 
