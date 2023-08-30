181a5e2e639ddee6fc5fd0744cfb45c56ed9d411
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-30 16:11:33 -0300

    creating public routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AuthLayout from './layouts/AuthLayout'
+import Login from './pages/Login'
+import Registrar from './pages/Registrar'
+import OlvidePassword from './pages/OlvidePassword'
+import NuevoPassword from './pages/NuevoPassword'
+import ConfirmarCuenta from './pages/ConfirmarCuenta'
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>UpTask</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Area publica */}
+        <Route path='/' element={<AuthLayout/>}>
+          <Route index element={<Login/>}/>
+          <Route path='registrar' element={<Registrar/>}/>
+          <Route path='olvide-password' element={<OlvidePassword/>}/>
+          <Route path='olvide-password/:token' element={<NuevoPassword/>}/>
+          <Route path='confirmar/:id' element={<ConfirmarCuenta/>}/>
+        </Route>
+        {/* Area privada */}
+        {/* <Route path='/'>
+
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
